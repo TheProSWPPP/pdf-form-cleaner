624bfda4326356643be6bf3e9522515aa82fc240
TheProSWPPP <dc@ProSWPPP.com>
2025-11-13 16:19:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -102,6 +102,3 @@ app.get('/health', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
